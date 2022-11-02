fed855ef863676184a09476aed7ee7bf3fa85341
Danylov2412 <daniiil1988@ukr.net>
2022-11-02 14:00:52 +0300

    2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,1 +1,19 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+const listEl = document.querySelector("#ingredients");
+const ingredEl = ingredients.map((ingredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = ingredient;
+  itemEl.classList.add("item");
+  console.log(itemEl);
+  return itemEl;
+});
+listEl.append(...ingredEl);
+
 
