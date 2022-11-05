91a34e5fc46f4b2510f329869618b5ef4cc6edfb
Danylov2412 <daniiil1988@ukr.net>
2022-11-05 12:12:59 +0400

    task5

---
+++ src/js/task-05.js
@@ -0,0 +1,21 @@
+const inputEl = document.querySelector('#name-input');
+const outputEl = document.querySelector('#name-output');
+inputEl.addEventListener('input', event => {
+    outputEl.textContent = event.currentTarget.value;
+    console.log(inputEl.value);
+    if (inputEl.value === ''){
+        outputEl.textContent = 'Anonymous'; 
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
