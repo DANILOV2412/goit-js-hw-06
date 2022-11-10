8f347b54cb346e1e139cef04ed995c0b680ff08d
Danylov2412 <daniiil1988@ukr.net>
2022-11-10 10:37:52 +0200

    task9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,33 @@
+
+
+const buttonChange = document.querySelector('.change-color');
+const textOutput = document.querySelector('color');
+document.body.classList.add('bgr.Color');
+
+buttonChange.addEventListener('click', onButtonChange);
+
+function onButtonChange(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+    textOutput.textContent = `#${Math.floor(Math.random() * 16777215).toString(
+    16,
+  )}`;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
