af3bd837fb3ba2bceb14822ff85ef1c6ef4d435a
Danylov2412 <daniiil1988@ukr.net>
2022-11-10 10:21:40 +0200

    task8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        return alert('Заполните пожалуйста пустые поля');
+    }
+
+    console.log({ Login: `${email.value}`, Password: `${password.value}` });
+    event.currentTarget.reset();
+
+}
+
 
